fe07a422555cafaee7accc166208a13651577379
Tselot Amare <tselotamare12@gmail.com>
2022-04-17 11:54:39 +0300

    Added all

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Topbar from './components/topbar/Topbar'
+function App() {
+  return (
+    <div>App
+      <Topbar/>
+    
+    </div>
+    
+  )
+}
+
+export default App
